3435e1940b736e9ba768a9243a9c1875a9376b6f
Matthias Wessendorf <matzew@apache.org>
2011-09-06 14:26:23 +0200

    initial commit of sample/demo code

---
+++ src/main/objc/Lib.h
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+
+@interface Lib : NSObject
+
+@end
---
+++ src/main/objc/Lib.m
@@ -0,0 +1,15 @@
+#import "Lib.h"
+
+@implementation Lib
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
